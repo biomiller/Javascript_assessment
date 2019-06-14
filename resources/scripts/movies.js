33f82e38c74c9e57c650006ed9ffd222af22b4fe
biomiller <92millero@gmail.com>
2019-06-14 15:27:48 +0100

    front end somewhat working

---
+++ resources/scripts/movies.js
@@ -0,0 +1,112 @@
+function test(){
+    console.log("test");
+}
+
+
+
+function makeRequest(method, url, body) {
+
+    return new Promise(
+        
+        (resolve, reject) => {
+
+        const req = new XMLHttpRequest();
+
+        req.onload = () => {
+
+            if (req.status >= 200 && req.status <= 299) {
+                resolve(req.responseText);
+            }
+            else {
+                const reason = new Error("Rejected");
+                reject(reason);
+            }
+        }
+        req.open(method, url);
+        req.send(body);
+    });
+    
+}
+
+// http://www.omdbapi.com/?apikey=603a192c&
+
+//http://www.omdbapi.com/?t=batman&y=2004
+
+// http://img.omdbapi.com/?apikey=603a192c&
+
+const buttonClickSearchMovies = () => {
+
+    sessionStorage.setItem('search',`http://www.omdbapi.com/?apikey=603a192c&s=${document.getElementById("searchTerm").value}`)
+
+    // console.log("test");
+    // return false;
+     makeRequest("GET", sessionStorage.getItem('search'))
+         .then((resolve) => {getMovies(resolve)})
+         .catch(function (error) { console.log(error.message) })
+};
+
+function getMovies(input) {
+
+    rawResponse = JSON.parse(input);
+
+    data = rawResponse.Search;
+   
+    console.log(data);
+
+    
+    for (let i = 0; i < data.length; i++) {
+
+
+        sessionStorage.setItem(('movie_'+i),(data[i].Title));
+
+        if (document.contains(document.getElementById("aMovieRow" + data[i].Title))) {
+            document.getElementById("moviesTable").removeChild(document.getElementById("aMovieRow" + data[i].Title));
+            }
+
+        let newRow = document.createElement('TR');
+
+        newRow.id = "aMovieRow" + data[i].Title;
+
+        document.getElementById("moviesTable").appendChild(newRow);
+
+        let td1 = document.createElement('TD');
+        document.getElementById("aMovieRow" + data[i].Title).appendChild(td1);
+        td1.innerText = data[i].Title;
+        
+        let td2 = document.createElement('TD');
+        document.getElementById("aMovieRow" + data[i].Title).appendChild(td2);
+        td2.innerText = data[i].Year;
+        
+        let td3 = document.createElement('TD');
+        document.getElementById("aMovieRow" + data[i].Title).appendChild(td3);
+        td3.innerText = data[i].Type;
+        
+        let td4 = document.createElement('TD');
+        let moreButton = document.createElement("button")
+        moreButton.id = data[i].Title
+        moreButton.type = "button";
+        moreButton.className = "btn";
+        moreButton.onclick=buttonMoreDetails;
+        moreButton.innerText = "More Details";
+        td4.appendChild(moreButton)
+        document.getElementById("aMovieRow" + data[i].Title).appendChild(td4);
+
+    }
+
+}
+
+const buttonMoreDetails = (e) => {
+    sessionStorage.setItem("title", e.target.getAttribute('id'));
+    location.href='more_details.html';
+};
+
+const getMoreDetails = () =>{
+
+    document.getElementById("movieTitle").innerText = sessionStorage.getItem('title');
+    document.getElementById('movieImage').src = `http://img.omdbapi.com/?apikey=603a192c&t=${sessionStorage.getItem('title')}`;
+
+}
+
+
+
+
